47d48e8c1fef2c7f0185a8356016b6f1603c6126
0xspringtime <0xspringtime@protonmail.com>
2023-02-24 11:13:58 -0500

    commit

--- matmul.py
+++ matmul.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
 
     #N^2 output cells with 2N compute each
-    flop = N*N*2*N
+    flop = N*N*2*N #The first N*N term represents the number of elements in one matrix, and the second N term represents the number of operations required to multiply each element in one row of the first matrix by each element in one column of the second matrix. The 2 term represents the fact that there are two summations performed for each element in the resulting matrix.
     print(f"{flop / 1e9:.2f} GFLOP")
 
     st = time.monotonic()
@@ -19,4 +19,4 @@ if __name__ == "__main__":
     et = time.monotonic()
     s = et-st
 
-    print(f"{flop/s * 1e-12:.2f} GFLOPS") 
+    print(f"{flop/s * 1e-12:.2f} TFLOP/S") 
